d99d884cbcdbe8d3f0dec1bfaed1ce675ee5bb9c
Jason Park <parkjs814@gmail.com>
2016-09-08 07:31:03 -0500

    exclude hotrepos.db

--- hotrepos/__init__.py
+++ hotrepos/__init__.py
@@ -3,9 +3,12 @@ import time
 import github
 import sns
 import db
+from datetime import datetime
 
 
 def job():
+    print '-----job started-----'
+    print datetime.utcnow()
     repos = github.get_hot_repos()
     for repo in repos:
         if not db.has_posted(repo):
