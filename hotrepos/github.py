b6db98087c6b60dca4cf6333929fc33fcbcc23e7
Jason Park <parkjs814@gmail.com>
2016-09-28 00:45:17 -0500

    mute

--- hotrepos/github.py
+++ hotrepos/github.py
@@ -5,12 +5,10 @@ import json
 
 def get_hot_repos(num=30):
     week_ago = (datetime.utcnow() - timedelta(days=7)).strftime("%Y-%m-%dT%H:%M:%S%z")
-    print week_ago
     response = requests.get('https://api.github.com/search/repositories?'
                             'q=created:>' + week_ago + '&'
                                                        'sort=stars&'
                                                        'order=desc')
-    print json.loads(response.text)
     repos = json.loads(response.text)['items'][:num]
     print 'fetched ' + repr(len(repos)) + ' repos'
     return repos
